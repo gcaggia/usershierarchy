77c37d47ecb65b0eedca15a6477a559bb8f02d3b
Guillaume Caggia <guillaume.caggia@gmail.com>
2018-11-24 22:39:32 +1100

    remove main.cr

--- main.cr
+++
@@ -1,7 +0,0 @@
-require "./solution"
-
-users = Array(User).from_json(File.read("data/users.json"))
-roles = Array(Role).from_json(File.read("data/roles.json"))
-organisation = Organisation.new users, roles
-
-puts organisation.getSubordinates(3).to_json
