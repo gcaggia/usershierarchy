c0b8d6056ff6864908c875fe7964e5467d1277a0
Guillaume Caggia <guillaume.caggia@gmail.com>
2018-11-24 20:29:08 +1100

    update api to accept user id as url param

--- api.cr
+++ api.cr
@@ -1,12 +1,19 @@
 require "kemal"
 require "./solution"
 
-get "/" do
+get "/api/home/" do |env|
+  env.response.content_type = "application/json"
+  {"Hello From Server"}.to_json
+end
+
+get "/api/get-subordinates/:user_id" do |env|
   users = Array(User).from_json(File.read("data/users.json"))
   roles = Array(Role).from_json(File.read("data/roles.json"))
   organisation = Organisation.new users, roles
   
-  organisation.getSubordinates(3).to_json
+  env.response.content_type = "application/json"
+  userId = env.params.url["user_id"].to_i
+  organisation.getSubordinates(userId).to_json
 end
 
 Kemal.run 8080
