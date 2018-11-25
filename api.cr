b2b2109d02626026530de908faef0ef4d8ea1893
Guillaume Caggia <guillaume.caggia@gmail.com>
2018-11-25 17:45:16 +1100

    add all tests for api application

--- api.cr
+++ api.cr
@@ -8,8 +8,10 @@ end
 
 get "/api/get-subordinates/:user_id" do |env|
   pathData = Kemal.config.env == "test" ? "spec" : "data"
+
   organisation = Organisation.new usersFilePath: "#{pathData}/users.json", 
                                   rolesFilePath: "#{pathData}/roles.json"
+                                  
   env.response.content_type = "application/json"
   userId = env.params.url["user_id"].to_i
   organisation.getSubordinates(userId).to_json
--- spec/api_spec.cr
+++ spec/api_spec.cr
@@ -14,9 +14,34 @@ describe "api" do
     response.status_code.should eq 200
   end
 
-  it "responds to /api/get-subordinates" do
+  it "should output 2 employees for Sam Supervisor user #3" do
+    get "/api/get-subordinates/3"
+    users = Array(User).from_json(response.body)
+    users.size.should eq 2
+  end
+
+  it "should output 'Emily Employee' and 'Steve Trainer' for Sam Supervisor user #3" do
+    get "/api/get-subordinates/3"
+    users = Array(User).from_json(response.body)
+    [users[0].name, users[1].name].sort.should eq ["Emily Employee", "Steve Trainer"]
+  end
+
+  it "should output 4 employees for Adam Admin user #1" do
     get "/api/get-subordinates/1"
-    response.status_code.should eq 200
+    users = Array(User).from_json(response.body)
+    users.size.should eq 4
+  end
+
+  it "should output [objUser2, objUser3, objUser4, objUser5] for Adam Admin user #1" do
+    get "/api/get-subordinates/1"
+    users = Array(User).from_json(response.body)
+    [users[0].id, users[1].id, users[2].id, users[3].id].sort.should eq [2, 3, 4, 5]
+  end
+
+  it "should output nothing for Emily Employee user #2" do
+    get "/api/get-subordinates/2"
+    users = Array(User).from_json(response.body)
+    users.size.should eq 0
   end
 
 end
