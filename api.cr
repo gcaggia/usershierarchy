81ec94b27f5569b351070c03b49e3dae5f1ec266
Guillaume Caggia <guillaume.caggia@gmail.com>
2018-11-25 18:20:41 +1100

    add comments to api

--- api.cr
+++ api.cr
@@ -1,19 +1,23 @@
 require "kemal"
 require "./solution"
 
+# Healthcheck of the API
 get "/api/home/" do |env|
   env.response.content_type = "application/json"
   "Hello From Server".to_json
 end
 
+# Main route for the API
 get "/api/get-subordinates/:user_id" do |env|
+  env.response.content_type = "application/json"
+  # Get data folder path
   pathData = Kemal.config.env == "test" ? "spec" : "data"
-
+  # Organisation is a list of users and roles
   organisation = Organisation.new usersFilePath: "#{pathData}/users.json", 
                                   rolesFilePath: "#{pathData}/roles.json"
-                                  
-  env.response.content_type = "application/json"
+  # Get user id from url parameter
   userId = env.params.url["user_id"].to_i
+  # Call method getSubordinates and return answer as json
   organisation.getSubordinates(userId).to_json
 end
 
